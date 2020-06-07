8eb884b24bab969d6a7751247e5835d6f72c228c
Vlad Kruume <Vladkruume5565@outlook.com>
2020-06-07 20:03:12 +0300

    test

--- SunSystemProject/SunSystemProject/FormTest.Designer.cs
+++ SunSystemProject/SunSystemProject/FormTest.Designer.cs
@@ -36,19 +36,19 @@
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.databaseSunSystemDataSet1 = new SunSystemProject.DatabaseSunSystemDataSet();
             this.label2 = new System.Windows.Forms.Label();
             this.buttonGameStart = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.buttonExitForm1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.times = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.databaseSunSystemDataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseSunSystemDataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // radioButton1
@@ -117,6 +117,7 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
@@ -128,6 +129,30 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(774, 358);
             this.panel1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(31, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ответить";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SunSystemProject.Properties.Resources.Earth;
+            this.pictureBox1.Location = new System.Drawing.Point(441, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 313);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // databaseSunSystemDataSet1
             // 
@@ -159,42 +184,6 @@
             this.buttonGameStart.Text = "Начать тест";
             this.buttonGameStart.UseVisualStyleBackColor = false;
             this.buttonGameStart.Click += new System.EventHandler(this.buttonGameStart_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::SunSystemProject.Properties.Resources.Earth;
-            this.pictureBox1.Location = new System.Drawing.Point(441, 18);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(309, 313);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(898, 41);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(24, 29);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "0";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.OrangeRed;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(31, 314);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 28);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Ответить";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // buttonExitForm1
             // 
@@ -238,6 +227,17 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(126, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0 / 8";
+            // 
             // FormTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -247,7 +247,6 @@
             this.Controls.Add(this.times);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.buttonExitForm1);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.buttonGameStart);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
@@ -258,8 +257,8 @@
             this.Text = "Тест";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.databaseSunSystemDataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseSunSystemDataSet1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -278,10 +277,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonGameStart;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttonExitForm1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label times;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
     }
 }
